505fd5d7a0d03d12560c0d88ca0a408d7f36b7eb
ljj <1061984275@qq.com>
2023-05-10 15:32:26 +0800

    第七天

--- gulimall-product/src/test/java/com/ljj/gulimall/produce/Demo.java
+++ gulimall-product/src/test/java/com/ljj/gulimall/produce/Demo.java
@@ -1,9 +1,5 @@
 package com.ljj.gulimall.produce;
 
-import com.aliyun.oss.ClientException;
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.OSSException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,9 +24,5 @@ public class Demo {
     // 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
     String objectName = "first";
 
-    // 创建OSSClient实例。
-    OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-        String content = "Hello OSS";
-        ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
 }
 }
--- gulimall-product/src/test/java/com/ljj/gulimall/produce/GulimallProduceApplicationTests.java
+++ gulimall-product/src/test/java/com/ljj/gulimall/produce/GulimallProduceApplicationTests.java
@@ -1,7 +1,6 @@
 package com.ljj.gulimall.produce;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClient;
+
 import com.ljj.gulimall.product.entity.BrandEntity;
 import com.ljj.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -16,15 +15,5 @@ import java.io.InputStream;
 
 @SpringBootTest
 public class GulimallProduceApplicationTests {
-
-    @Autowired
-    private OSS oss;
-    @Test
-    public void uploadTest() throws FileNotFoundException {
-        InputStream inputStream = new FileInputStream("F:\\images\\image-20220301152038894.png");
-        oss.putObject("xyc-ljj","1",inputStream);
-        System.out.println("成功");
-
-    }
 
 }
